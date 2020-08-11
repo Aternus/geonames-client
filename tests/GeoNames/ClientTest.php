080c8be758abaa46f7efc43fe5a667f602386351
Martin Rüegg <martin.rueegg@metaworx.ch>
2020-08-11 15:09:06 +0200

    [TASK] Add tests for paramsToQueryString() including arrays in parameters

--- tests/GeoNames/ClientTest.php
+++ tests/GeoNames/ClientTest.php
@@ -54,6 +54,59 @@ final class ClientTest extends TestCase
     }
 
 
+    public function testParamsToQueryString()
+    {
+        $g = $this->client;
+
+        $class = new \ReflectionClass($g);
+        $method = $class->getMethod('paramsToQueryString');
+        $method->setAccessible(true);
+
+        $paramsToQueryString = static function (array $params = []) use ($method, $g) {
+            return $method->invokeArgs($g, [$params]);
+        };
+
+        $params = [];
+        $qs = $paramsToQueryString($params);
+        $this->assertEquals('', $qs);
+
+        $params =['q' => 'foo'];
+        $qs = $paramsToQueryString($params);
+        $this->assertEquals('q=foo', $qs);
+
+        $params =['q' => ['foo', 'bar']];
+        $qs = $paramsToQueryString($params);
+        $this->assertEquals('q=foo&q=bar', $qs);
+
+        $params =['name_equals' => 'Grüningen', 'country' => 'CH'];
+        $qs = $paramsToQueryString($params);
+        $this->assertEquals('name_equals=Gr%C3%BCningen&country=CH', $qs);
+
+        $params =['name_equals' => 'Grüningen', 'country' => ['CH']];
+        $qs = $paramsToQueryString($params);
+        $this->assertEquals('name_equals=Gr%C3%BCningen&country=CH', $qs);
+
+        $arr = $g->search($params);
+        $this->assertIsArray($arr);
+        $this->assertArrayHasKey(0, $arr);
+
+        $params =['name_equals' => 'Grüningen', 'country' => ['CH', 'DE']];
+        $qs = $paramsToQueryString($params);
+        $this->assertEquals('name_equals=Gr%C3%BCningen&country=CH&country=DE', $qs);
+
+        $arr = $g->search($params);
+        $this->assertIsArray($arr);
+        $this->assertArrayHasKey(0, $arr);
+
+        $params =['name_equals' => 'Grüningen', 'country' => []];
+        $qs = $paramsToQueryString($params);
+        $this->assertEquals('name_equals=Gr%C3%BCningen', $qs);
+
+        $arr = $g->search($params);
+        $this->assertIsArray($arr);
+        $this->assertArrayHasKey(0, $arr);
+    }
+
     public function testAstergram()
     {
         $obj = $this->client->astergdem([
